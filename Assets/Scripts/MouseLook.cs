fdd24962409209baafe9d6af15e29886f1a5a16d
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2021-10-27 00:18:12 +0300

    Player added

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float rotationX = 0f;
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update() {
+        MoveCamera();
+    }
+    private void MoveCamera() {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
