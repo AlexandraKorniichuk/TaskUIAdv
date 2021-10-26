dac9426754f3fc0eacf0d867f91a4b2e639d9279
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2021-10-26 23:58:40 +0300

    Scenes and terrain added

---
+++ Assets/Scripts/ChangingScaleButtons.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChangingScaleButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    public Animator _animator;
+    private void Awake() {
+        _animator = GetComponent<Animator>();
+    }
+    public void OnPointerEnter(PointerEventData eventData) {
+        _animator.SetBool("IsMouseOnTheButton", true);
+    }
+    public void OnPointerExit(PointerEventData eventData) {
+        _animator.SetBool("IsMouseOnTheButton", false);
+    }
+}
---
+++ Assets/Scripts/QuitingGame.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class QuitingGame : MonoBehaviour {
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
