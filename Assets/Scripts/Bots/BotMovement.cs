c75aa8c590e623c8f1440b1a0abb94b9f6a4a209
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2021-10-28 00:37:32 +0300

    Bot movement added

---
+++ Assets/Scripts/Bots/BotMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BotMovement : MonoBehaviour {
+    public GameObject player;
+    public float speed = 4f;
+
+    void Update() {
+        transform.LookAt(player.transform);
+        transform.position += transform.forward * Time.deltaTime * speed;
+    }
+}
