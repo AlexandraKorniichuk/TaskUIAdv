4bd5299e367e06d0710142ccdb0c4e06eba767e6
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2021-10-28 16:52:33 +0300

    EndGame scene

---
+++ Assets/Scripts/Menu/EndScoreText.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScoreText : MonoBehaviour {
+    private float score;
+    private void Awake() {
+        score = PlayerPrefs.GetFloat("Score");
+    }
+    void Start() {
+        gameObject.GetComponent<Text>().text = $"Score: {score}";
+    }
+}
